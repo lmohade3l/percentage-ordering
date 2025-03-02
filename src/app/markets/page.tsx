1666c81553fc7c3ce39841f2291c408639b16478
lmohade3l <mohade3.2710@gmail.com>
2025-03-02 20:15:48 +0330

    install shadcn + table

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,5 +1,15 @@
 "use client"
 import useSWR from "swr"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  
 
 export default function Markets() {
     const fetcher = (...args: [string, RequestInit?]) => fetch(...args).then(res => res.json())
@@ -8,8 +18,28 @@ export default function Markets() {
     console.log({ data })
     
     return (
-        <div>
+        <div dir="rtl" className="text-right">
             صفحه بازارها
+
+            <Table dir="rtl" className="text-right">
+              <TableCaption>لیست فاکتورهای اخیر شما.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="text-right">فاکتور</TableHead>
+                  <TableHead className="text-right">وضعیت</TableHead>
+                  <TableHead className="text-right">روش</TableHead>
+                  <TableHead className="text-right">مبلغ</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-right">INV001</TableCell>
+                  <TableCell className="text-right">پرداخت شده</TableCell>
+                  <TableCell className="text-right">کارت اعتباری</TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
         </div>
     )
 }
