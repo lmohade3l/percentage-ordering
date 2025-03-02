0bf1e9bf5425e6cf45a75c556e05298efd08a44f
lmohade3l <mohade3.2710@gmail.com>
2025-03-02 20:00:11 +0330

    install swr

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+import useSWR from "swr"
+
 export default function Markets() {
+    const fetcher = (...args: [string, RequestInit?]) => fetch(...args).then(res => res.json())
+    const { data, error } = useSWR("https://api.bitpin.ir/v1/mkt/markets/", fetcher)
+    
+    console.log({ data })
+    
     return (
         <div>
-            hi markets
+            صفحه بازارها
         </div>
     )
 }
